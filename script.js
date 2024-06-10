fd002a845b713e7d204a08880b47008bee5cd074
João Carlos de Melo Inferdes <106244163+JaocHollyday@users.noreply.github.com>
2024-06-10 11:25:09 -0300

    aula 4

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const caixaPerguntas = document.querySelector("caixa-perguntas")
 const caixaAlternativas = document.querySelector("caixa-alternativa")
 const caixaResultado = document.querySelector("caixa-resultado")
 const textoResultado = document.querySelector("texto-resultado")
+ 
 
 const perguntas = [
     {
