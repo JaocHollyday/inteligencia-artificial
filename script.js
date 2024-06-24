ce6e62d0d8df3511035a016ad6d5951f13a66aec
João Carlos de Melo Inferdes <106244163+JaocHollyday@users.noreply.github.com>
2024-06-24 10:26:43 -0300

    aula 6

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const caixaPrincipal = document.querySelector("caixa-principal")
-const caixaPerguntas = document.querySelector("caixa-perguntas")
-const caixaAlternativas = document.querySelector("caixa-alternativa")
-const caixaResultado = document.querySelector("caixa-resultado")
-const textoResultado = document.querySelector("texto-resultado")
- 
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-Alternativas");
+const caixaResultado = document.querySelector(".caixa-Resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
 const perguntas = [
     {
-        enunciado: "ssim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
             "Isso é assustador!",
             "Isso é maravilhoso!"
@@ -20,7 +20,7 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Após a elaboração do trabalho escrito, o professor realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
         alternativas: [
             "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
             "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
@@ -30,11 +30,11 @@ const perguntas = [
         enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
         alternativas: [
             "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-            "riar uma imagem utilizando um gerador de imagem de IA."
+            "Criar uma imagem utilizando um gerador de imagem de IA."
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
         alternativas: [
             "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
             "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
@@ -42,22 +42,33 @@ const perguntas = [
     }
 ];
 
-let atual = 0;f
+let atual = 0;
 let perguntaAtual;
 
-function mostraPergunta () {
-    perguntaAtual = perguntas[atual]
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     mostraAlternativas();
 }
 
 function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas){
-        const botaoAlternativas = document.createElement("button")
-        botaoAlternativas.textContent = alternativa;
-        caixaAlternativas.appendChild(botaoAlternativas);
-        
-
+for(const alternativa of perguntaAtual.alternativas){
+    const botaoAlconst caixaPrincipal = document.querySelector("caixa-principal")
+    const caixaPerguntas = document.querySelector("caixa-perguntas")
+    const caixaAlternativas = document.querySelector("caixa-alternativa")
+    const caixaResultado = document.querySelector("caixa-resultado")
+    const textoResultado = document.querySelector("texto-resultado")
+     
+    const perguntas = [
+        {
+            enunciado: "ssim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+            alternativas: [
+    …    }
     }
+    mostraPergunta();ternativas = document.createEvent("button");
+    botaoAlternativas.textContent = alternativa;
+    caixaAlternativas.appendChild(botaoAlternativas);
 }
+}
+
 mostraPergunta();
