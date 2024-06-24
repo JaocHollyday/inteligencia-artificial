b74384755a91ff565152a4656c50b4b0e2aa09c0
João Carlos de Melo Inferdes <106244163+JaocHollyday@users.noreply.github.com>
2024-06-24 11:19:18 -0300

    s

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const perguntas = [
             {
                 texto:"Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
                 afirmacao: "afirmacao"
-            }
+            } ,
             {
             texto:"Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema."
             afirmacao:"afirmacao"
@@ -38,7 +38,7 @@ const perguntas = [
             {
             texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
             afirmacao: "afirmacao"
-            }
+            } ,
             {   
             texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
             afirmacao: "afirmacao"
@@ -51,7 +51,7 @@ const perguntas = [
             {
                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
                afirmacao: "afirmacao"
-            }
+            } ,
             {
                texto: "Criar uma imagem utilizando um gerador de imagem de IA."
                afirmacao: "afirmacao"
@@ -65,7 +65,7 @@ const perguntas = [
          {
             texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
             afirmacao: "afirmacao"
-         }
+         } ,
          {
             texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
             afirmacao: "afirmacao"
